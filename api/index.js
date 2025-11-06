1ab55d67f8b3670bee1a576797a53c9463f1d5b3
tamannashah-work <ts.work.1809@gmail.com>
2025-11-06 13:12:25 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,6 @@ const app = express();
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(express.json());
